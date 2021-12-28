f7581e3d270e5520b30ba65e3b2bea1a9d5b2a90
wilgnerschuertz <60265271+wilgnerschuertz@users.noreply.github.com>
2021-12-28 13:15:12 -0400

    Frases do Dia (App)

--- Dev/frases do dia/lib/main.dart
+++ Dev/frases do dia/lib/main.dart
@@ -1,9 +1,86 @@
-import "package:flutter/material.dart";
+import 'package:flutter/material.dart';
+import 'dart:math';
 
-
-void main() {
+void main(){
   runApp(MaterialApp(
-    title: "Frases do dia",
-    home: Container(color: Colors.white,),
+    home: Home(),
+    debugShowCheckedModeBanner: false,
   ));
+}
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  var _frases = [
+    "Frase 1",
+    "Frase 2",
+    "Frase 3",
+    "Frase 4",
+    "Frase 5",
+  ];
+
+  var _fraseGerada = "Clique Abaixo";
+
+  void _gerarFrase(){
+
+    var numeroSorteado = Random().nextInt(_frases.length);
+    print(numeroSorteado);
+
+    setState(() {
+      _fraseGerada = _frases[numeroSorteado];
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Frases do Dia"),
+        backgroundColor: Colors.lightBlueAccent,
+      ),
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.all(16), // Espaçamento nas laterais.
+          //width: double.infinity,
+          /*decoration: BoxDecoration(
+              border: Border.all(width: 3, color: Colors.amber)
+          ),*/
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget> [
+              Image.asset("images/logo.png"),
+              Text(
+                _fraseGerada,
+                textAlign: TextAlign.justify,
+                style: TextStyle(
+                    fontSize: 17,
+                    fontStyle: FontStyle.italic,
+                    color: Colors.black
+                ),
+              ),
+              ElevatedButton(
+                child: Text(
+                  "Clique Aqui",
+                  style: TextStyle(
+                      fontSize: 15,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w500
+                  ),
+                ),
+
+                onPressed: _gerarFrase,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
